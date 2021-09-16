662fd553146b6c48c590fc76e7fbc9a96681ed1a
Kadek Pradnyana <kadek.pradnyana@ir.com>
2021-09-16 21:58:51 +0800

    Refactoring

--- src/controller.ts
+++ src/controller.ts
@@ -1,9 +1,8 @@
 import SocMedService from "./services/SocMedService";
 import Instagram from "./instagram/Instagram";
 import { ioInput } from "./libs/utils";
-import Likes from "./models/Likes";
-import Comments from "./models/Comments";
-import ProgressBar from 'progress';
+import Likes, { ILike } from "./models/Likes";
+import Comments, { IComment } from "./models/Comments";
 import ora from 'ora';
 
 export default async function controller(browserInstance: Promise<any>) {
@@ -20,7 +19,6 @@ export default async function controller(browserInstance: Promise<any>) {
   const postShortcode = await ioInput('Input the post shortcode => ', true);
   const validTaggedPeople = await ioInput('Number of valid tagged people => ', true);
   const numberOfWinner = Number(await ioInput('Number of respected winner => ', true));
-  const bar = new ProgressBar('scraping :bar', { total: 3 });
 
   const spinner = ora();
   spinner.spinner = 'bouncingBar';
@@ -31,13 +29,12 @@ export default async function controller(browserInstance: Promise<any>) {
     dbName: 'instagram.com'
   });
   const likes = await session.getPostLikes(postShortcode);
-  const allLikes = likes.reduce((prev, next) => prev.concat(next.data), []) as any[];
+  const allLikes = likes.reduce((prev, next) => prev.concat(next.data), []) as ILike[];
   await likesDB.saveLikes(postShortcode, likes);
 
-  bar.tick();
-
   if (!allLikes.length) {
-    bar.interrupt('Opps! No likes! We can\'t decide it yet.');
+    spinner.stop();
+    console.log('Opps! No likes! We can\'t decide it yet.');
     return process.exit();
   }
 
@@ -46,48 +43,53 @@ export default async function controller(browserInstance: Promise<any>) {
     dbName: 'instagram.com'
   });
   const comments = await session.getPostComments(postShortcode);
-  const allComments = comments.reduce((prev, next) => prev.concat(next.data), []) as any[];
+  const allComments = comments.reduce((prev, next) => prev.concat(next.data), []) as IComment[];
   await commentsDB.saveComments(postShortcode, comments);
-  bar.tick();
 
   if (!allComments.length) {
-    bar.interrupt('Opps! No comments! We can\'t decide it yet.');
+    spinner.stop();
+    console.log('Oops! No comments! We can\'t decide it yet.');
     return process.exit();
   }
 
   // get the comments text
   spinner.text = 'Filtering user with valid number of tags.';
   const onlyValidComments = allComments.filter((comment) => commentsDB.countTaggedPeople(comment.text, Number(validTaggedPeople)));
-  bar.tick();
 
   if (!onlyValidComments.length) {
-    bar.interrupt('Oops! Seems like we haven\'t found the winner yet.');
+    spinner.stop();
+    console.log('Oops! Seems like we haven\'t found the winner yet.');
     return process.exit();
   }
 
   const validUserWithCommentLike = onlyValidComments.filter((comment) => allLikes.some((like) => like.username === comment.owner.username));
 
   if (!validUserWithCommentLike.length) {
-    bar.interrupt('Oops! Seems no one is followed the rules.');
+    spinner.stop();
+    console.log('Oops! Seems no one is followed the rules.');
     return process.exit();
   }
 
   spinner.text = `Selecting the ${numberOfWinner} of respected winner! The candidates: ${validUserWithCommentLike.map((user) => user.owner.username).join(', ')}`;
-  bar.tick();
 
-  const theWinners = [];
-  if (validUserWithCommentLike.length < numberOfWinner) {
-    for (let i = 0; i < numberOfWinner; i++) {
-      const winner = validUserWithCommentLike[Math.floor(Math.random() * validUserWithCommentLike.length)];
-      theWinners.push(winner);
-    }
-  } else {
-    theWinners.push(...validUserWithCommentLike);
-  }
+  const theWinners = chooseWinners(validUserWithCommentLike, numberOfWinner);
 
   spinner.text = `The winner is: ${theWinners.map((winner) => winner.owner.username).join(', ')}`;
 
-  bar.tick();
   spinner.stop();
   process.exit();
+}
+
+function chooseWinners(userComments: IComment[], numberOfWinner: number) {
+  const theWinners = [];
+  if (userComments.length < numberOfWinner) {
+    for (let i = 0; i < numberOfWinner; i++) {
+      const winner = userComments[Math.floor(Math.random() * userComments.length)];
+      theWinners.push(winner);
+    }
+  } else {
+    theWinners.push(...userComments);
+  }
+
+  return theWinners;
 }
